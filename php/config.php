87318e4cd46030f63d21749641de053b2ddd24ce
Yan Gorshtenin <support@netlodge.net>
2014-05-23 07:08:19 -0700

    Commit #1

---
+++ php/config.php
@@ -0,0 +1,23 @@
+<?php
+
+error_reporting(0);
+
+/*
+
+PHP Radio DB v.1.0
+
+*/
+
+require_once('php/functions.php');
+
+// General Settings
+
+$api_key           = ''; // here you should put the API key you got from http://dirble.com/login/
+$installation_path = ''; // eg: http://domain.com/radio/ or http://domain.com/ (don`t forget to add a slash at the ending and www/http:// at the begining)
+
+// Site settings
+
+$site_name             = 'PHP Radio DB';
+$site_slogan           = 'PHP Radio DB is a PHP script wich allows you to find new radio stations around the internet.';
+$site_meta_description = 'PHP Radio DB is a PHP script wich allows you to find new radio stations around the internet.'; 
+
